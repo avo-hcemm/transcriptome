3ee2804c8b99b204179282ba03dddbdcac9bff5a
avo-hcemm <alessandra.vittorini@hcemm.eu>
2026-03-05 18:37:27 +0100

    GeneOverlap: counting of overlaps without duplicates

--- src/analysis/GeneOverlap.java
+++ src/analysis/GeneOverlap.java
@@ -15,11 +15,14 @@ public class GeneOverlap {
 		
 		Iterator<IntervalTree.Node<Gene>> iter = geneTree.iterator();
 		
+		HashSet<String> processedGeneSet = new HashSet<>();
 		while(iter.hasNext()) {
 			Gene gene = iter.next().getValue();
 			int geneStart = gene.getStart();
 			int geneEnd = gene.getEnd();
 			String geneName = gene.getGeneName();
+			
+			if(processedGeneSet.contains(geneName))continue;
 			
 			Iterator<IntervalTree.Node<Gene>> overlappingGenes =
 	                 geneTree.overlappers(geneStart, geneEnd);
@@ -27,17 +30,28 @@ public class GeneOverlap {
 			HashSet<String> geneSet = new HashSet<>();
 			 while(overlappingGenes.hasNext()) {
 				 Gene ogene = overlappingGenes.next().getValue();
-				 if(gene.getStrand() != ogene.getStrand())
-					 geneSet.add(ogene.getGeneName());
+				 String ogeneName = ogene.getGeneName();
+				 if (!geneName.equals(ogeneName) 
+		                    && gene.getStrand() != ogene.getStrand()) {
+					 	processedGeneSet.add(ogeneName);
+		                geneSet.add(ogeneName);
+		            }
 			 }
-			 overlappingGenesMap.put(geneName,geneSet );
+			 overlappingGenesMap.put(geneName,geneSet);
 		}
 		return overlappingGenesMap;
 		 
 	}
 	
 	public static void printOverlaps(HashMap<String, HashSet<String>> overlappingGenesMap) {
-		overlappingGenesMap.forEach((g,o) -> System.out.println(g+" "+o));
+		int count = 0;
+		for (HashMap.Entry<String, HashSet<String>> entry : overlappingGenesMap.entrySet()) {
+	        String gene = entry.getKey();
+	        HashSet<String> overlapSet = entry.getValue();
+	        System.out.println(gene + " " + overlapSet);
+	        if(overlapSet.size() != 0) count = count + overlapSet.size();
+	    }
+		System.out.println("Number of overlapping protein-coding genes:"+ count);
 	}
 	
 }
